f2b0e5b3af5e719b4ad661ac31ec97a3cc5a1863
Giovanni Spatafora <141187369+giovaspat@users.noreply.github.com>
2023-12-31 19:52:29 +0100

    Add files via upload

---
+++ server.ts
@@ -0,0 +1,61 @@
+import express from 'express';
+import "express-async-errors";
+import morgan from "morgan";
+import dotenv from "dotenv";
+
+dotenv.config()
+
+const app = express()
+const port = 3000
+
+type Planet = {
+    id: number,
+    name: string,
+  };
+
+  type Planets = Planet[];
+
+  let planets: Planets = [
+    {
+      id: 1,
+      name: "Earth",
+    },
+    {
+      id: 2,
+      name: "Mars",
+    },
+        {
+      id: 3,
+      name: "Jupiter",
+    },
+    {
+      id: 4,
+      name: "Venus",
+    },
+    {
+      id: 5,
+      name: "Mercury",
+    },
+    {
+      id: 6,
+      name: "Uranus",
+    },
+    {
+      id: 7,
+      name: "Neptune",
+    },
+    {
+      id: 8,
+      name: "Saturn",
+    },
+  ];
+
+app.get('/planets', (req, res) => {
+  res.send(planets)
+})
+
+app.use(morgan("dev"))
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
