d3d093cc37aebbba216fd70ae383c935c8475aa4
Giovanni Spatafora <141187369+giovaspat@users.noreply.github.com>
2024-01-10 23:39:52 +0100

    Add files via upload

---
+++ dist/controller/planets.js
@@ -0,0 +1,72 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+import Joi from "joi";
+import { db } from "../db.js";
+let planets = [
+    {
+        id: 1,
+        name: "Earth",
+    },
+    {
+        id: 2,
+        name: "Mars",
+    },
+];
+const getAll = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const planets = yield db.many(`SELECT * FROM planets;`);
+    console.log(planets);
+    res.status(200).json(planets);
+});
+const getOneById = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { id } = req.params;
+    const planet = yield db.one(`SELECT * FROM planets WHERE id=$1;`, Number(id));
+    res.status(200).json(planet);
+});
+const planetScheme = Joi.object({
+    id: Joi.number().integer().required(),
+    name: Joi.string().required(),
+});
+const create = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { id, name } = req.body;
+    const newPlanet = { id: id, name };
+    const newPlanetValidation = planetScheme.validate(newPlanet);
+    if (newPlanetValidation.error) {
+        return res.status(400).json({ msg: newPlanetValidation.error.details[0].message });
+    }
+    else {
+        yield db.none(`INSERT INTO planets (name) VALUES ($1)`, name);
+        res.status(201).json({ msg: "The planet was created" });
+    }
+});
+const createImage = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    var _a;
+    const { id } = req.params;
+    const fileName = (_a = req.file) === null || _a === void 0 ? void 0 : _a.path;
+    if (fileName) {
+        db.none(`UPDATE planets SET image=$2 WHERE id=$1`, [id, fileName]);
+        res.status(201).json({ msg: "Planet image uploaded" });
+    }
+    else {
+        res.status(400).json({ msg: "Planet uploading failed" });
+    }
+});
+const updateById = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { id } = req.params;
+    const { name } = req.body;
+    yield db.none(`UPDATE planets SET name=$2 WHERE id=$1`, [id, name]);
+    res.status(201).json({ msg: "The planet was updated" });
+});
+const deleteById = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { id } = req.params;
+    yield db.none(`DELETE FROM planets WHERE id=$1`, Number(id));
+    console.log(planets);
+    res.status(201).json({ msg: "The planet was deleted" });
+});
+export { getAll, getOneById, create, createImage, updateById, deleteById };
---
+++ dist/server.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import 'express-async-errors';
+import morgan from 'morgan';
+import { getAll, getOneById, create, createImage, updateById, deleteById } from "./controller/planets.js";
+import { logIn } from "./controller/users.js";
+import multer from "multer";
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "./uploads");
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    },
+});
+const upload = multer({ storage });
+const app = express();
+const port = 3000;
+app.use(morgan('dev'));
+app.use(express.json());
+app.get("/api/planets", getAll);
+app.get("/api/planets/:id", getOneById);
+app.post("/api/planets", create);
+app.put("/api/planets/:id", updateById);
+app.delete("/api/planets/:id", deleteById);
+app.post("/api/planets/:id/image", upload.single("image"), createImage);
+app.post("/api/users/login", logIn);
+app.listen(port, () => {
+    console.log(`Example app listening on port http://localhost:${port}`);
+});
